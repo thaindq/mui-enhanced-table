bcddb7447ca14fce5e4ddf9f0ca291f0bb4f88db
Thai Nguyen <nguyenthaidq@gmail.com>
2023-04-10 15:25:57 -0700

    Update dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,6 +3,14 @@ module.exports = {
         '@storybook/addon-docs',
         '@storybook/addon-essentials',
         '@storybook/addon-storysource',
+        '@storybook/addon-mdx-gfm',
     ],
-    stories: ['../stories/**/*.stories.@(j|t)sx'],    
+    stories: ['../stories/**/*.stories.@(j|t)sx'],
+    framework: {
+        name: '@storybook/react-webpack5',
+        options: {},
+    },
+    docs: {
+        autodocs: true,
+    },
 };
